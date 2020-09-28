fb041b6d28892c7b891fa612c344516524e690d6
Daniel Vera <daniel.vera1@virginiogomez.cl>
2020-09-28 12:35:18 -0300

    fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,6 +10,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
  
@@ -19,15 +20,62 @@ export class ProductosService {
   
   private CargarProductos(){
 
-    this.http.get('https://angular-html-14103.firebaseio.com/productos_idx.json')
+    return new Promise((resolve, reject) =>{
+
+      this.http.get('https://angular-html-14103.firebaseio.com/productos_idx.json')
   
-    .subscribe((resp: Producto[]) => {
-
-      console.log(resp);
-      this.productos = resp;
-      this.cargando = false;
-
+      .subscribe((resp: Producto[]) => {
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+  
+      });
     });
+  }
+
+    getProducto (id: string){
+
+      return this.http.get(`https://angular-html-14103.firebaseio.com/productos/${id}.json`)
+
+    }
+
+    buscarProducto(termino: string){
+
+
+      if(this.productos.length === 0){
+        //cargar productos
+
+        this.CargarProductos().then(()=>{
+          //ejecutar despues de tener los productos
+          //aplicar filtro
+
+          this.filtrarProductos(termino)
+        });
+
+      }else{
+        //aplicar filtro
+        this.filtrarProductos(termino)
+      }
+    }
+
+    private filtrarProductos(termino: string){
+
+     
+      this.productosFiltrado = [];
+
+      termino = termino.toLocaleLowerCase();
+
+
+      this.productos.forEach(prod =>{
+
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if(prod.categoria.indexOf(termino) >=0 || tituloLower.indexOf(termino) >=0 ){
+          this.productosFiltrado.push(prod)
+
+        }
+
+      })
     }
   }
 
